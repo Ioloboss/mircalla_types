f33600dab9b0622940e71a48ccc50f1ba42228ac
Harrison Jones <harrisoniolojones@gmail.com>
2026-02-19 17:37:05 +0000

    First Commit

---
+++ src/units.rs
@@ -0,0 +1,98 @@
+// ===================================================
+// PIXELS
+// ===================================================
+
+
+#[derive(Debug, PartialEq, Copy, Clone, PartialOrd)]
+pub struct Pixels<T> {
+	pub value: T,
+}
+
+impl <T> Pixels<T>
+where
+	T: Into<f32> + Copy
+{
+	pub fn to_screen_space(&self, screen_dimension: Pixels<f32>) -> ScreenSpace {
+		(((self.value.into() / screen_dimension.value) * 2.0) - 1.0).into()
+	}
+
+	pub fn to_screen_space_length(&self, screen_dimension: Pixels<f32>) -> ScreenSpace {
+		((self.value.into() / screen_dimension.value) * 2.0).into()
+	}
+}
+
+impl <T> From<T> for Pixels<T> {
+	fn from(value: T) -> Self {
+		Self { value }
+	}
+}
+
+impl <A, B> std::ops::Add<Pixels<B>> for Pixels<A>
+where
+	A: std::ops::Add + Copy,
+	B: Into<A> + Copy,
+	<A as std::ops::Add>::Output: Copy,
+{
+	type Output = Pixels<<A as std::ops::Add>::Output>;
+
+	fn add(self, rhs: Pixels<B>) -> Self::Output {
+		Self::Output { value: self.value + rhs.value.into() }
+	}
+}
+
+impl<A, B> std::ops::AddAssign<Pixels<B>> for Pixels<A>
+where
+	Pixels<A>: std::ops::Add<Pixels<B>, Output = Pixels<A>>,
+	B: Copy,
+	A: Copy,
+{	
+	fn add_assign(&mut self, rhs: Pixels<B>) {
+		*self = *self + rhs;
+	}
+}
+
+impl <A, B> std::ops::Sub<Pixels<B>> for Pixels<A>
+where
+	A: std::ops::Sub + Copy,
+	B: Into<A> + Copy,
+	<A as std::ops::Sub>::Output: Copy,
+{
+	type Output = Pixels<<A as std::ops::Sub>::Output>;
+	fn sub(self, rhs: Pixels<B>) -> Self::Output {
+		Self::Output { value: self.value - rhs.value.into() }
+	}
+}
+
+impl std::ops::Mul<usize> for Pixels<f32> {
+	type Output = Pixels<f32>;
+	fn mul(self, rhs: usize) -> Self::Output {
+		(self.value * rhs as f32).into()
+	}
+}
+
+impl std::ops::Div<usize> for Pixels<f32> {
+	type Output = Pixels<f32>;
+	fn div(self, rhs: usize) -> Self::Output {
+		(self.value / rhs as f32).into()
+	}
+}
+
+
+// ===================================================
+// SCREEN SPACE
+// ===================================================
+
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct ScreenSpace {
+	pub value: f32,
+}
+
+impl <T> From<T> for ScreenSpace
+where
+	T: Into<f32>
+{
+	fn from(value: T) -> Self {
+		Self { value: value.into() }
+	}
+}
---
+++ src/vectors.rs
@@ -0,0 +1,229 @@
+use std::collections::btree_map::Values;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+	Vertical,
+	Horizontal,
+}
+
+
+// ===================================================
+// POSITION
+// ===================================================
+
+#[derive(Debug, Clone, Copy)]
+pub struct Position<T> {
+	pub x: T,
+	pub y: T,
+}
+
+impl <A, B> From<(A, A)> for Position<B>
+where
+	A: Into<B>
+{
+	fn from(value: (A, A)) -> Self {
+		Self { x: value.0.into(), y: value.1.into() }
+	}
+}
+
+impl <A, B> std::ops::Add<Position<B>> for Position<A>
+where
+	A: std::ops::Add<B>,
+{
+	type Output = Position<<A as std::ops::Add<B>>::Output>;
+	fn add(self, rhs: Position<B>) -> Self::Output {
+		Self::Output {
+			x: self.x + rhs.x,
+			y: self.y + rhs.y,
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Axis {
+	X,
+	Y,
+}
+
+impl From<Dimension> for Axis {
+	fn from(value: Dimension) -> Self {
+		match value {
+			Dimension::Width => Self::X,
+			Dimension::Height => Self::Y,
+		}
+	}
+}
+
+impl <T> Position<T>
+where
+	T: Copy,
+{
+	pub fn get(&self, axis: Axis) -> T {
+		match axis {
+			Axis::X => self.x,
+			Axis::Y => self.y,
+		}
+	}
+
+	pub fn set(&mut self, axis: Axis, value: T) {
+		match axis {
+			Axis::X => self.x = value,
+			Axis::Y => self.y = value,
+		}
+	}
+}
+
+impl <T> Position<Option<T>> {
+	pub fn unwrap_contents(self) -> Position<T> {
+		Position {
+			x: self.x.unwrap(),
+			y: self.y.unwrap(),
+		}
+	}
+
+	pub fn none() -> Self {
+		Self {x : None, y: None }
+	}
+}
+
+
+
+
+
+
+// ===================================================
+// SIZE
+// ===================================================
+
+#[derive(Debug, Clone, Copy)]
+pub struct Size<T> {
+	pub width: T,
+	pub height: T,
+}
+
+impl <A, B> From<(A, A)> for Size<B>
+where
+	A: Into<B>
+{
+	fn from(value: (A, A)) -> Self {
+		Self { width: value.0.into(), height: value.1.into() }
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Dimension {
+	Width,
+	Height,
+}
+
+impl From<Direction> for Dimension {
+	fn from(value: Direction) -> Self {
+		match value {
+			Direction::Horizontal => Dimension::Width,
+			Direction::Vertical => Dimension::Height,
+		}
+	}
+}
+
+impl Dimension {
+	pub fn opposite(&self) -> Self {
+		match self {
+			Dimension::Width => Dimension::Height,
+			Dimension::Height => Dimension::Width,
+		}
+	}
+}
+
+
+impl <T> Size<T>
+where
+	T: Copy,
+{
+	pub fn get(&self, dimension: Dimension) -> T {
+		match dimension {
+			Dimension::Width => self.width,
+			Dimension::Height => self.height,
+		}
+	}
+
+	pub fn set(&mut self, dimension: Dimension, value: T) {
+		match dimension {
+			Dimension::Width => self.width = value,
+			Dimension::Height => self.height = value,
+		}
+	}
+}
+
+impl <T> Size<Option<T>> {
+	pub fn unwrap_contents(self) -> Size<T> {
+		Size {
+			width: self.width.unwrap(),
+			height: self.height.unwrap(),
+		}
+	}
+
+	pub fn none() -> Self {
+		Size { width: None, height: None}
+	}
+}
+
+
+
+
+// ===================================================
+// COLOUR
+// ===================================================
+
+fn to_linear_rgb(color_chanel: u8) -> f32 {
+	let value = color_chanel as f32 / 255.0;
+	if value > 0.04045 {
+		((value + 0.055) / 1.055).powf(2.4)
+	} else {
+		value / 12.92
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Colour {
+	pub r: f32,
+	pub g: f32,
+	pub b: f32,
+}
+
+impl Colour {
+	pub fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+		Self {
+			r: to_linear_rgb(r),
+			g: to_linear_rgb(g),
+			b: to_linear_rgb(b),
+		}
+	}
+}
+
+impl Colour {
+	pub fn black() -> Self {
+		Colour { r: 0.0, g: 0.0, b: 0.0 }
+	}
+
+	pub fn red() -> Self {
+		Colour { r: 1.0, g: 0.0, b: 0.0 }
+	}
+
+	pub fn green() -> Self {
+		Colour { r: 0.0, g: 1.0, b: 0.0 }
+	}
+
+	pub fn blue() -> Self {
+		Colour { r: 0.0, g: 0.0, b: 1.0 }
+	}
+}
+
+impl Into<[f32; 3]> for Colour {
+	fn into(self) -> [f32; 3] {
+		[
+			self.r,
+			self.g,
+			self.b,
+		]
+	}
+}
